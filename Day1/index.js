8dd223bc9303b5dcfba1f4027b597e3aec60d3fd
mubeen207 <mubeenulislam207@gmail.com>
2026-08-26 00:36:16 +0500

    refactor: uncomment code for user input and result calculation

--- Day1/index.js
+++ Day1/index.js
@@ -1,5 +1,15 @@
-let num1 = 10;
-const num2 = 20;
+// let num1 = 10;
+// const num2 = 20;
 
-console.log(num1 + num2);
+// console.log(num1 + num2);
 
+function add(a, b) {
+  return a + b;
+}
+
+let a = +prompt("Enter First Number");
+let b = +prompt("Enter Second Number");
+
+let result = add(a, b);
+
+alert(result);
