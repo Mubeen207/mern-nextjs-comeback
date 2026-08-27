20114bf67e9dca82a52adde27c7749507a34cf90
mubeen207 <mubeenulislam207@gmail.com>
2026-08-27 14:51:44 +0500

    fix: correct random index generation in idgenerator function

--- Day2/script.js
+++ Day2/script.js
@@ -14,7 +14,7 @@ let idgenerator = () => {
     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   let id = "";
   for (let i = 0; i < 6; i++) {
-    let rendomIndex = Math.floor(Math.random * chars.lemght);
+    let rendomIndex = Math.floor(Math.random() * chars.length);
     id += chars[rendomIndex];
   }
   return id;
