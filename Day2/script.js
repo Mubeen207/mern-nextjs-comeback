59db27963127bf9ebbb66e810e3278c8007682f4
mubeen207 <mubeenulislam207@gmail.com>
2026-08-27 17:25:25 +0500

    fix: refactor addTask function for improved task handling and clarity

--- Day2/script.js
+++ Day2/script.js
@@ -11,29 +11,36 @@ let addTask = () => {
   }
   warrnigElement.innerHTML = "";
   if (isEditId !== "") {
-    inputButton.innerHTML = "Add";
     tasks = tasks.map((item) => {
       if (isEditId === item.id) {
         return { ...item, task: inputValue.value };
       }
+      return item;
     });
+    isEditId = "";
+    inputButton.innerHTML = "Add";
+    inputValue.value = "";
+    pushTasks(tasks);
+  } else {
+    tasks.push({
+      task: inputValue.value,
+      id: idgenerator(),
+    });
+    inputValue.value = "";
+    pushTasks(tasks);
   }
-  tasks.push({
-    task: inputValue.value,
-    id: idgenerator(),
-  });
-  inputValue.value = "";
-  pushTasks(tasks);
 };
 
 let pushTasks = (tasks) => {
-  allTasksElement.innerHTML = tasks.map((task) => {
-    return `<div id="${task.id}">
+  allTasksElement.innerHTML = tasks
+    .map((task) => {
+      return `<div id="${task.id}">
   <p>${task.task}</p>
   <button onClick="deleteTask('${task.id}')">Delete</button>
   <button onClick="editTask('${task.id}')">Edit</button>
 </div>`;
-  });
+    })
+    .join("");
 };
 let deleteTask = (id) => {
   let updatedTasks = tasks.filter((task) => task.id !== id);
@@ -45,11 +52,10 @@ let deleteTask = (id) => {
 let editTask = (id) => {
   isEditId = id;
   inputButton.innerHTML = "Update";
-  tasks.map((item) => {
-    if (item.id === id) {
-      return (inputValue.value = item.task);
-    }
-  });
+  let targetTask = tasks.find((item) => item.id === id);
+  if (targetTask) {
+    inputValue.value = targetTask.task;
+  }
 };
 
 let idgenerator = () => {
